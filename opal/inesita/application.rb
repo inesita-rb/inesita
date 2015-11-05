c1b74f9e1e938b4c9c162c68527c17f521dfc3bf
Michał Kalbarczyk <fazibear@gmail.com>
2015-11-06 00:04:42 +0100

    cleanup application

--- opal/inesita/application.rb
+++ opal/inesita/application.rb
@@ -3,24 +3,36 @@ module Inesita
     include Inesita::Component
 
     def initialize(options = {})
-      router = options[:router]
-      layout = options[:layout]
-      store  = options[:store]
-
-      fail Error, 'Router missing' unless router
-      fail Error, "Invalid #{router} class, should mixin Inesita::Router" unless router.include?(Inesita::Router)
-      fail Error, "Invalid #{layout} class, should mixin Inesita::Layout" if layout && !layout.include?(Inesita::Layout)
-      fail Error, "Invalid #{store} class, should mixin Inesita::Store" if store && !store.include?(Inesita::Store)
-
-      @router = router.new
-      @layout = layout.new if layout
-
-      @root   = @layout ? @layout : @router
-      @store  = store.new.with_root_component(@root).store if store
+      setup_router(options[:router])
+      setup_layout(options[:layout])
+      setup_store(options[:store])
     end
 
     def render
       component @root
+    end
+
+    private
+
+    def setup_router(router)
+      fail Error, 'Router missing' unless router
+      fail Error, "Invalid #{router} class, should mixin Inesita::Router" unless router.include?(Inesita::Router)
+      @router = router.new
+    end
+
+    def setup_layout(layout)
+      if layout
+        fail Error, "Invalid #{layout} class, should mixin Inesita::Layout" unless layout.include?(Inesita::Layout)
+        @root = layout.new
+      else
+        @root = @router
+      end
+    end
+
+    def setup_store(store)
+      return unless store
+      fail Error, "Invalid #{store} class, should mixin Inesita::Store" unless store.include?(Inesita::Store)
+      @store = store.new.with_root_component(@root).store
     end
   end
 end
