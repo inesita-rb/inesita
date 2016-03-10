920d1b52905194d4ca881254706ebc72679c1f4d
Michał Kalbarczyk <fazibear@gmail.com>
2016-03-10 15:00:57 +0100

    cleanup build

--- lib/inesita/cli/build.rb
+++ lib/inesita/cli/build.rb
@@ -33,45 +33,52 @@ class InesitaCLI < Thor
                 desc: 'source (app) dir'
 
   def build
-    assets = Inesita::Server.new({
-      dist: true,
-      static_dir: options[:static_dir],
-      app_dir: options[:app_dir],
-      app_dist_dir: options[:app_dist_dir]
-    }).assets_app
-
-    build_dir = options[:destination_dir]
-    force = options[:force]
+    assets = assets_server
 
     empty_directory build_dir, force: force
 
-    copy_static(static_dir, build_dir, force)
-    create_index(build_dir, assets['index.html'].source, force)
-    create_js(build_dir, assets['application.js'].source, force)
-    create_css(build_dir, assets['stylesheet.css'].source, force)
+    copy_static
+    create_index(assets)
+    create_js(assets)
+    create_css(assets)
   end
 
   no_commands do
-    def copy_static(static_dir, build_dir, force)
-      Dir.glob('./#{static_dir}/**/*').each do |file|
+    def assets_server
+      Inesita::Server.new({
+        dist: true,
+        static_dir: options[:static_dir],
+        app_dir: options[:app_dir],
+        app_dist_dir: options[:app_dist_dir]
+      }).assets_app
+    end
+
+    def copy_static
+      Dir.glob("./#{options[:static_dir]}/**/*").each do |file|
         if File.directory?(file)
-          empty_directory File.join(build_dir, file), force: force
+          empty_directory File.join(options[:destination_dir], file), force: options[:force]
         else
-          copy_file File.absolute_path(file), File.join(build_dir, file), force: force
+          copy_file File.absolute_path(file), File.join(options[:destination_dir], file), force: options[:force]
         end
       end
     end
 
-    def create_index(build_dir, html, force)
-      create_file File.join(build_dir, 'index.html'), Inesita::Minify.html(html), force: force
+    def create_index(assets)
+      create_file File.join(options[:destination_dir], 'index.html'),
+                  Inesita::Minify.html(assets['index.html'].source),
+                  force: options[:force]
     end
 
-    def create_js(build_dir, javascript, force)
-      create_file File.join(build_dir, 'application.js'), Inesita::Minify.js(javascript),  force: force
+    def create_js(assets)
+      create_file File.join(options[:destination_dir], 'application.js'),
+                  Inesita::Minify.js( assets['application.js'].source),
+                  force: options[:force]
     end
 
-    def create_css(build_dir, stylesheet, force)
-      create_file File.join(build_dir, 'stylesheet.css'), Inesita::Minify.css(stylesheet), force: force
+    def create_css(assets)
+      create_file File.join(options[:destination_dir], 'stylesheet.css'),
+                  Inesita::Minify.css( assets['stylesheet.css'].source),
+                  force: options[:force]
     end
   end
 end
