686e32b55b362a1ff68047e9727173b432e84396
Michał Kalbarczyk <fazibear@gmail.com>
2016-03-16 11:40:02 +0100

    add init to component

--- opal/inesita/component_virtual_dom_extension.rb
+++ opal/inesita/component_virtual_dom_extension.rb
@@ -12,10 +12,12 @@ module Inesita
       fail Error, "Component is nil in #{self.class} class" if comp.nil?
       @__virtual_nodes__ ||= []
       @__virtual_nodes__ << cache_component(comp) do
-        (comp.is_a?(Class) ? comp.new : comp)
+        comp = (comp.is_a?(Class) ? comp.new : comp)
           .with_root_component(@root_component)
           .with_router(@router)
           .with_store(@store)
+        comp.init
+        comp
       end.with_props(opts[:props] || {}).render_virtual_dom
     end
   end
