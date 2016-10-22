bbcdb2cab4762846f59c674ab699cc23c66179b9
Michał Kalbarczyk <fazibear@gmail.com>
2016-10-22 19:35:36 +0200

    Fix deprecated store for compatibility

--- opal/inesita/store.rb
+++ opal/inesita/store.rb
@@ -1,9 +1,15 @@
 module Inesita
+  # TODO: Remove it.
   module Store
-    # TODO: Remove it.
     def self.included(base)
       $console.warn('"include Inesita::Store" is deprecated. Use "include Inesita::Injection" insted.')
-      include Injection
+      self.include Injection
+    end
+
+    def render!
+      Browser.animation_frame do
+        root_component.render_if_root
+      end
     end
   end
 end
