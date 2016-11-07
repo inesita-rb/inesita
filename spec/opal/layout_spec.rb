413475d30f3e4c3cc4e3d8ae46f4105918fa9c05
Michał Kalbarczyk <fazibear@gmail.com>
2016-11-07 19:01:21 +0100

    Remove old specs

--- spec/opal/layout_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'spec_helper'
-
-describe Inesita::Layout do
-  let(:layout) { Class.new { include Inesita::Layout } }
-
-  it 'should respond to #outlet' do
-    expect(layout.new).to respond_to(:outlet)
-  end
-end
